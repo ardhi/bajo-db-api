59965470acdfe648a5f96fb94c7fb4f836ca6fbe
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-24 06:50:53 +0700

    Updates

--- bajoDb/method/record/clear.js
+++ bajoDb/method/record/clear.js
@@ -1,6 +1,6 @@
 async function clear ({ schema, options = {} } = {}) {
   const { error } = this.bajo.helper
-  throw error('\'%s\' is unsupported', 'recordClear')
+  throw error('\'%s\' is unsupported', 'collClear')
 }
 
 export default clear
