feac349d28905fda1617f5a7ebadad861baa7b51
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-17 21:02:30 +0700

    plugin -> ns

--- bajoDb/boot/driver.js
+++ bajoDb/boot/driver.js
@@ -3,11 +3,11 @@ async function driver () {
   const { isString } = this.app.bajo.lib._
   const type = ['bajo', 'custom']
   const driver = 'restproxy'
-  await eachPlugins(async function ({ file, plugin }) {
+  await eachPlugins(async function ({ file, ns }) {
     const cfg = readJson(file)
     if (!cfg.type) return undefined
     if (isString(cfg.type)) cfg.type = [cfg.type]
-    cfg.type = cfg.type.map(t => `${t}@${plugin}`)
+    cfg.type = cfg.type.map(t => `${t}@${ns}`)
     type.push(...cfg.type)
   }, { glob: 'boot/driver.json', baseNs: this.name })
   return { type, driver }
